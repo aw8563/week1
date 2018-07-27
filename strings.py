f71500c8d9e2f3933ba3d2bf12a3ec994beea502
Andy <z5161179@student.unsw.edu.au>
2018-07-27 17:56:27 +1000

    q3

--- strings.py
+++ strings.py
@@ -1,6 +1,9 @@
 strings = ['This', 'list', 'is', 'now', 'all', 'together']
 sentence = ''
 for i in range(0, 6):
+
     sentence = sentence + strings[i] + ' ' 
 
 print (sentence)
+
+print(' '.join(strings))
